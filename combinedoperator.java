1b5ec05edc6f5e99aa55e73bc04e842251bc7fe3
Pratik-Devadhe <pratik180264@gmail.com>
2025-02-05 14:19:19 +0530

    Add files via upload

---
+++ combinedoperator.java
@@ -0,0 +1,19 @@
+class combinedoperator{
+
+	public static void main(String s[]){
+	
+		int a = 10;
+		int b = 20;
+		
+		
+		a += b;
+		b -= a;
+		a -= b; 
+		
+		System.out.println(a);
+		System.out.println(b);
+	
+	
+	}
+
+}
