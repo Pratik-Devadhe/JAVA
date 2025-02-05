fae1a5b1750f1e9afe59be8d24e19448905338f6
Pratik-Devadhe <pratik180264@gmail.com>
2025-02-05 14:01:53 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,36 @@
+public class SwitchCase{
+
+
+public static void main(String s[]){
+
+
+int a = 1;
+
+while(a != 5){
+switch(a){
+
+case 1: 
+	System.out.println(a);
+	a =2;
+	break;
+case 2: 
+	System.out.println(a);
+	a =5;
+	break;
+default : 
+	System.out.println(a);
+
+}
+
+}
+
+}
+
+
+
+
+
+
+
+
+}
